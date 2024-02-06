6d758139c79da4da0e9086dfa9b31816a5995e1c
Nepalcem <mark.emailtest@gmail.com>
2024-02-06 17:28:17 +0200

    blank project with packages

--- src/components/globalStyle.ts
+++ src/components/globalStyle.ts
@@ -1,3 +1,6 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
 body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
@@ -5,9 +8,22 @@ body {
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  background-color: #f5f5f5;
 }
 
 code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
+
+#root {
+  display: flex;
+  justify-content: center;
+}
+
+p,
+ul {
+  margin: 0;
+  padding: 0;
+}
+`;
